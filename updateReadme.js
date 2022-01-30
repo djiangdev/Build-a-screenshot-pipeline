fa30748bf6d7d2efee3fcf3378e0b4cfc125ff70
Nguyen Dinh Giang <dinhgiang2611@gmail.com>
2022-01-31 02:18:19 +0700

    add update Readme file

---
+++ updateReadme.js
@@ -0,0 +1,27 @@
+// Include node fs (file stream) and https modules
+const fs = require('fs')
+const https = require('https')
+
+function readWriteAsync () {
+  // Create string of markdown to be inserted
+  const articles = `\n ![alt text](https://raw.githubusercontent.com/djiangdev/Build-a-screenshot-pipeline/main/screenshot.png)\n \n`
+
+  // Update README using FS
+  fs.readFile('README.md', 'utf-8', (err, data) => {
+    if (err) {
+      throw err
+    }
+
+    // Write the new README
+    fs.writeFile('README.md', updatedMd, 'utf-8', err => {
+      if (err) {
+        throw err
+      }
+
+      console.log('README update complete.')
+    })
+  })
+}
+
+// Call the function
+readWriteAsync()
