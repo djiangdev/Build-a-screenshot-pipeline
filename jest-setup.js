45e86b3a90c115522a2a76017835e0fa5821c1fa
Nguyen Dinh Giang <dinhgiang2611@gmail.com>
2022-01-31 03:07:39 +0700

    edit

--- jest-setup.js
+++ jest-setup.js
@@ -3,6 +3,7 @@ const customConfig = { threshold: 0 }
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
   customDiffConfig: customConfig,
   failureThreshold: 55,
-  failureThresholdType: 'percent'
+  failureThresholdType: 'percent',
+  updatePassedSnapshot: true
 })
 expect.extend({ toMatchImageSnapshot })
