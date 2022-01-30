61948ee3a004f8a97353a65ce7c69daf010416b8
Nguyen Dinh Giang <dinhgiang2611@gmail.com>
2022-01-31 01:34:12 +0700

    noColors : true

--- __test__/puppeteer.spec.js
+++ __test__/puppeteer.spec.js
@@ -12,7 +12,8 @@ describe('jest-image-snapshot', () => {
     await page.goto('https://nifty-booth-0bf80f.netlify.app/')
     const image = await page.screenshot({ path: 'screenshot.png' })
     expect(image).toMatchImageSnapshot({
-      updatePassedSnapshot: true
+      updatePassedSnapshot: true,
+      noColors: true
     })
   })
 
